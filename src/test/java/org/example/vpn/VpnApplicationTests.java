5fdbba1c8e6ef45d758c71df954acbf221566a61
Vladimir <v.dementev@bftcom.com>
2026-06-07 17:40:22 +0700

    Initial commit

---
+++ src/test/java/org/example/vpn/VpnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
